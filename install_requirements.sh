4a16ecf84743d7794bf017faffea377f55511d49
Skyner <reskyner@googlemail.com>
2017-08-25 17:41:27 +0100

    added scipy to requirements

--- install_requirements.sh
+++ install_requirements.sh
@@ -2,4 +2,6 @@ pip install 'matplotlib>=1.3.1'
 pip install 'numpy>=1.11.0'
 pip install 'pandas>=0.18.1'
 pip install 'tkintertable>=1.2'
+pip install 'SciPy>=0.13.3'
 pip install 'scikit_learn>=0.19.0'
+
---
+++ lib/python2.7/locale.py
@@ -0,0 +1,1 @@
+/dls_sw/prod/tools/RHEL6-x86_64/Python/2-7-3/prefix/lib/python2.7/locale.py
