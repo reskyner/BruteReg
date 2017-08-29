56ca7447177145184f8e4ce3854c6f3fff0eba69
reskyner <reskyner@googlemail.com>
2017-08-29 16:54:44 +0100

    removed example job script

--- example_job.sh
+++
@@ -1,5 +0,0 @@
-test
-cd /dls/science/groups/i04-1/software/BruteReg_dev/BruteReg
-source /dls/science/groups/i04-1/software/BruteReg_dev/BruteReg/bin/activate
-./bin/python2.7 brutereg.py -i ./input_files/hfe_descriptors.csv -o test.csv
-deactivate
